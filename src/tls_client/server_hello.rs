ed2ddee05713bedf232e8690f51472e3dc60cccf
Trevor <trevortomlin2@outlook.com>
2023-06-07 12:25:32 -0700

    Added test for server hello parsing pubkey

--- src/tls_client/server_hello.rs
+++ src/tls_client/server_hello.rs
@@ -28,3 +28,22 @@ pub fn parse_pubkey(server_hello: &[u8]) -> PublicKey {
     PublicKey::from(pubkey)
     
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_pubkey() {
+
+        let server_hello = include_str!("../test/server_hello.txt").replace(" ", "");
+        let server_hello = hex::decode(server_hello).unwrap();
+
+        let pubkey = parse_pubkey(&server_hello);
+
+        let expected_key: [u8; 32] = [0x9f, 0xd7, 0xad, 0x6d, 0xcf, 0xf4, 0x29, 0x8d, 0xd3, 0xf9, 0x6d, 0x5b, 0x1b, 0x2a, 0xf9, 0x10, 0xa0, 0x53, 0x5b, 0x14, 0x88, 0xd7, 0xf8, 0xfa, 0xbb, 0x34, 0x9a, 0x98, 0x28, 0x80, 0xb6, 0x15];
+        assert_eq!(pubkey.as_bytes(), &expected_key);
+
+    }
+
+}
